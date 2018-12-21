0b497ddb34bf1895f375e9307c924736d000ea1c
CodeGorilla <you@example.com>
2018-12-21 12:50:18 +0100

    hackathon project added

---
+++ hackathonproject/demo.js
@@ -0,0 +1,48 @@
+function myFunction()
+{
+alert ("De bedoeling is dat als je op 'Submit' drukt,\n dat dan de gegevens door worden gelinkt.\n Hier een voorbeeld hoe dat kan.");
+
+}
+
+var images=[
+    
+"desk.jpg",
+ "room.jpg"
+];
+
+
+var num=0
+
+function next(){
+
+var slider=document.getElementById("slider");
+
+num++;
+
+if(num>=images.length){
+
+
+ num=0;
+}
+    
+slider.src=images[num];
+    
+}
+
+
+
+
+function prev(){
+var slider=document.getElementById("slider");
+
+num--;
+
+if(num<0){
+
+ num=image.length-1;
+    
+}
+    
+ slider.src=images[num];
+    
+}
